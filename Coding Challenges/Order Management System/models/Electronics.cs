d67d6731f246f506a1d215d1f6901b546c7f449b
AlhanCreation <alhanapsiddique@gmail.com>
2025-04-09 14:34:28 +0530

    oms

---
+++ Coding Challenges/Order Management System/models/Electronics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementsystem.entity
+{
+    internal class Electronics : Product
+    {
+
+        public string Brand { get; set; }
+        public int WarrantyPeriod { get; set; }
+
+        public Electronics(string productName, string description, double price, int quantityInStock, string type, string brand, int warrantyPeriod)
+            : base(productName, description, price, quantityInStock, type)
+        {
+            Brand = brand;
+            WarrantyPeriod = warrantyPeriod;
+        }
+        public override string ToString()
+        {
+            return $"Brand::{Brand}\nWarranty::{WarrantyPeriod}\n";
+        }
+
+    }
+}
---
+++ Coding Challenges/Order Management System/util/DBUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data.SqlTypes;
+
+
+namespace OrderManagementSystem.util
+{
+    public class DBUtil
+    {
+        public static SqlConnection GetDBConn()
+        {
+            SqlConnection conn = new SqlConnection("Data Source=HP\\SQLEXPRESS;Initial Catalog=oms;Integrated Security=True");
+            conn.Open();
+            return conn;
+        }
+    }
+}
